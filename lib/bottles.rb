ac9ebd8bb3efec3d53f91fed43a26d4ba445e3b1
xDOASx <benesbit@yahoo.com>
2019-05-15 11:22:56 -0700

    Create method to handle multiple number params

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,24 +3,38 @@
 class Bottles
   attr_accessor :this_verse
 
-  def verse(number)
-    if (number == 1)
-      last_verse()
-    else
-
-      this_verse = "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"
-
-      if (number > 2)
-        this_verse << "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-      else
-        this_verse << "Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
-      end
-
-      this_verse
-    end
+  def initialize
+    this_verse = ""
   end
 
-  def last_verse()
+  def verse(number)
+    return second_to_last_verse if number == 1
+    return third_to_last_verse if number == 2
+    return final_verse if number == 0
+
+    this_verse = "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"
+    this_verse << "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+
+    this_verse
+  end
+
+  def verses(num1, num2)
+    @ret_verse = verse(num1)
+    @ret_verse << "\n"
+    @ret_verse << verse(num2)
+  end
+
+  def final_verse
+    this_verse = "No more bottles of beer on the wall, no more bottles of beer.\n"
+    this_verse << "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  end
+
+  def third_to_last_verse
+    this_verse = "2 bottles of beer on the wall, 2 bottles of beer.\n"
+    this_verse << "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+  end
+
+  def second_to_last_verse
     this_verse = "1 bottle of beer on the wall, 1 bottle of beer.\n"
     this_verse << "Take it down and pass it around, no more bottles of beer on the wall.\n"
   end
