2c3692bd5653d15cce30e17a7100ef80a0841390
xDOASx <benesbit@yahoo.com>
2019-05-20 11:40:01 -0700

    Add default parameter to container

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -24,13 +24,17 @@ class Bottles
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "#{number-1} bottle of beer on the wall.\n"
+      "#{number-1} #{container} of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
       "#{number} bottles of beer.\n" +
       "Take one down and pass it around, " +
       "#{number-1} bottles of beer on the wall.\n"
     end
+  end
+
+  def container(number = nil)
+    'bottle'
   end
 
 end
