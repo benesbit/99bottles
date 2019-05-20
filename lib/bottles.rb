65e7064ec9d98f43acfa3905a02266d4b2086b1f
xDOASx <benesbit@yahoo.com>
2019-05-20 11:15:39 -0700

    SHAMELESS GREEN. Copy Yong's code.
    Copy Yong's code so that we have a shameless green, passing all tests despite the need for a refactor.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,41 +1,36 @@
-# bottles.rb
-
 class Bottles
-  attr_accessor :this_verse
 
-  def initialize
-    this_verse = ""
+  def song
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).map {|i| verse(i)}.join("\n")
   end
 
   def verse(number)
-    return second_to_last_verse if number == 1
-    return third_to_last_verse if number == 2
-    return final_verse if number == 0
-
-    this_verse = "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"
-    this_verse << "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-
-    this_verse
+    case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of beer on the wall.\n"
+    end
   end
 
-  def verses(num1, num2)
-    @ret_verse = verse(num1)
-    @ret_verse << "\n"
-    @ret_verse << verse(num2)
-  end
-
-  def final_verse
-    this_verse = "No more bottles of beer on the wall, no more bottles of beer.\n"
-    this_verse << "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-  end
-
-  def third_to_last_verse
-    this_verse = "2 bottles of beer on the wall, 2 bottles of beer.\n"
-    this_verse << "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-  end
-
-  def second_to_last_verse
-    this_verse = "1 bottle of beer on the wall, 1 bottle of beer.\n"
-    this_verse << "Take it down and pass it around, no more bottles of beer on the wall.\n"
-  end
 end
