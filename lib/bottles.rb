1475ab1d3c93be112f5b4e02be4a78b7bccb53e9
xDOASx <benesbit@yahoo.com>
2019-05-22 10:44:30 -0700

    REFACTORING2. Using new class.
    Instantiate new class and using an object of that class and its methods.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,7 +8,8 @@ class Bottles
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    bottle_number = BottleNumber.new
+    "#{bottle_number.quantity(number).capitalize} #{container(number)} of beer on the wall, " +
     "#{quantity(number)} #{container(number)} of beer.\n" +
     "#{action(number)}, " +
     "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
